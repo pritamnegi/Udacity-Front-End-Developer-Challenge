2a93ff929531243bfd98135effd8aa59bf8ae518
Pritam Negi <pritamdnegi@gmail.com>
2018-02-27 21:26:58 +0530

    Added Lesson 14: Javascript functions Quiz

---
+++ Lesson 14/laugh_it_off.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+function laugh() {
+    var my_laugh = "hahahahahahahahahaha!";
+    return my_laugh;
+}
+
+
+console.log(/* call the laugh function */laugh());
